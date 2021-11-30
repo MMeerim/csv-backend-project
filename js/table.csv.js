122305f3d108603191ad8f15d454cd3cafa73c8a
MMeerim <musabaeva.meerim@gmail.com>
2021-11-29 19:22:58 -0600

    read any csv

---
+++ js/table.csv.js
@@ -0,0 +1,62 @@
+export default class {
+    /**
+     * @param {HTMLTableElement} root  The table element which will display the CSV data. 
+     */
+   constructor(root) {
+       this.root = root;
+    
+   }
+   
+   /**
+    * Clears existing data in the table and replaces it with the new data.
+    * 
+    * @param {string[][]} data A 2D array of data to be used as the table body
+    * @param {string{}} headerColumns List of headings to be used
+    */
+   update(data, headerColumns = []) {
+       this.clear();
+       this.setHeader(headerColumns);
+       this.setBody(data);
+   }
+
+   /**
+    * Clears all contents of the table (including the header).
+    */
+
+   clear() {
+       this.root.innerHTML = "";
+   }
+   /**
+    * Sets the table header.
+    *
+    * @param {string[]} headerColumns List of headings to be used
+    */
+    setHeader(headerColumns) {
+        this.root.insertAdjacentHTML("afterbegin", `
+            <thead>
+                <tr>
+                    ${ headerColumns.map(text => `<th>${text}</th>`).join("") }
+                </tr>
+            </thead>
+        `);
+    }
+
+    setBody(data) {
+        const rowsHTML = data.map(row => {
+            return `
+                <tr>
+                    ${ row.map (text => `<td>${ text }</td>` ).join("") }
+                </tr>
+            `;
+        } );
+        
+        this.root.insertAdjacentHTML( "beforeend", 
+        `
+            <tbody>
+                ${ rowsHTML.join("") }
+            </tbody>
+        `
+        );
+    }
+
+}
